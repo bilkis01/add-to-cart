46816d734f3136ecd660ec6302d55f9f6428050a
bilkis01 <bilkiscse@gmail.com>
2023-01-02 13:23:14 +0600

    product destructuring

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Cart.css'
+import Items from './Items';
+import {products} from './Products';
 
 const Cart = () => {
+
+    const [item,setItem] = useState(products);
+
     return (
-        <div>
-            
+       <>
+       <header>
+        <div className="continue-shopping">
+            <img src="./images/arrow.png" alt="arrow"  className="arrow-icon"/>
+            <h3>Continue Shopping</h3>
         </div>
+
+        <div className="cart-icon">
+            <img src="./images/cart.png" alt="cart" />
+            <p>7</p>
+        </div>
+       </header>
+
+       <section className="main-cart-section">
+        <h1>Shopping Cart</h1>
+        <p className='total-items'>You have <span className='total-items-count'>7</span> items in shopping cart</p>
+
+        <div className="cart-items">
+            <div className="cart-items-container">
+
+                {
+                    item.map((curItem)=>{
+                        return(
+                            <Items key={curItem.id} {...curItem}></Items>
+                    )
+                    })
+                }
+         
+                
+           
+            </div>
+        </div>
+        <div className="card-total">
+            <h3>Cart Total :  <span> 22000 tk </span></h3>
+            <button>Checkout</button>
+        </div>
+       </section>
+       
+       </>
     );
 };
 
--- src/component/Items.js
+++ src/component/Items.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const Items = () => {
+const Items = ({title,description,img,price,quantity}) => {
     return (
-        <div>
+        <>
+            <div className="items-info">
+                    <div className="product-img">
+                        <img src={img} alt="" />
+                    </div>
+                    <div className="title">
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                    </div>
+                    <div className="add-minus-quantity">
+                    <i class="fa-solid fa-minus minus"></i>
+                    <input type="text" placeholder='2' />
+                    <i class="fa-solid fa-plus add"></i>
+                    </div>
+                    <div className="price">
+                        <h3>{price} tk </h3>
+                    </div>
+                    <div className="remove-item">
+                    <i class="fa-solid fa-trash-can remove"></i>
+                    </div>
+                </div>
+                <hr />
             
-        </div>
+        </>
     );
 };
 
